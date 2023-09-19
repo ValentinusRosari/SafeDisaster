ba1df2e1fc6fe4a99c0b986c98e8bbb9ac5881dd
valent <valentinus.wastu@gmail.com>
2023-09-19 18:26:05 +0700

    menambahkan class "action"

--- SafeDisaster/Class.cs
+++ SafeDisaster/Class.cs
@@ -70,4 +70,26 @@ namespace SafeDisaster{
             Console.WriteLine($"Phone Number: {PhoneNumber}");
         }
     }
+
+    class Action{
+        //property
+        private string earthquakeInstruction =
+        "Instruksi ketika terjadi gempa bumi:\n1. Melindungi kepala dengan menggunakan bantal atau helm, atau berdirilah di bawah pintu.\n2. Berlindung di bawah meja untuk menghindari dari benda-benda yang dimungkinkan akan jatuh seperti atap atau benda berbahaya lainnya.\n3. Bila keluar rumah, perhatikan kemungkinan pecahan kaca, genteng atau material lain. Tetap lindungi kepala anda dan segera menuju ke lapangan terbuka.\n4. Jangan berdiri di dekat tiang, pohon atau sumber listrik atau gedung yang mungkin roboh.\n5. Kenali bagian bangunan gedung atau rumah yang memiliki struktur kuat, seperti pada sudut bangunan untuk berlindung.\n6. Ikuti instruksi evakuasi dari pengelola, penjaga, atau petugas yang berwenang.\n7. Pilihlah menggunakan tangga darurat untuk melakukan evakuasi keluar bangunan. Apabila sedang berada di dalam elevator, tekan semua tombol atau gunakan interphone untuk melakukan panggilan kepada pengelola gedung.";
+        private string floodInstruction =
+        "Instruksi ketika terjadi banjir:\n1. Melakukan evakuasi diri dan keluarga ke tempat yang lebih tinggi atau lebih aman.\n2. Menyiapkan peralatan yang diperlukan selama evakuasi, seperti senter, korek api, alat penerangan dan peralatan darurat lainnya.\n3. Menyiapkan makanan kering atau instan.\n4. Menggunakan sepatu boot dan sarung tangan.\n5. Menyiapkan berbagai macam obat-obatan yang diperlukan untuk melakukan pertolongan pertama.";
+        private string fireInstruction =
+        "Instruksi ketika terjadi kebakaran:\n1. Segera dapatkan alat pemadam api.\n2. Beritahu orang sekitar\n3. Tinggalkan barang berharga jika tidak bisa lagi diselamatkan.\n4. Cari tempat evakuasi yang aman.\n5. Segera hubungi petugas pemadam kebakaran";
+
+        //method
+        public void ShowEarthquakeInstruction(){
+            Console.WriteLine(earthquakeInstructions);
+        }
+        public void ShowFloodInstruction(){
+            Console.WriteLine(floodInstructions);
+        }
+        public void ShowFireInstruction(){
+            Console.WriteLine(fireInstructions);
+        }
+
+    }
 }
