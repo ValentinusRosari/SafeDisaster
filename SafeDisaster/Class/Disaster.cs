03ed2cdb32730b220befdae174951adaf2636184
dutaramadhan <duta2711@gmail.com>
2023-09-19 20:12:58 +0700

    feat: add action, disaster, user, and weather class

---
+++ SafeDisaster/Class/Disaster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeDisaster.Class
+{
+    internal class Disaster
+    {
+        private string _type;
+        private string _location;
+        private DateTime _time;
+        private string _magnitude;
+
+        public string Type { 
+            get { return _type; }
+            set { _type = value; }
+        }
+        public string Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+        public DateTime Time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+        public string Magnitude
+        {
+            get { return _magnitude; }
+            set { _magnitude = value; }
+        }
+        public Disaster(string type, string location, DateTime time, string magnitude)
+        {
+            this.Type = type;
+            this.Location = location;
+            this.Time = time;
+            this.Magnitude = magnitude;
+        }
+        public Disaster GetDisaster(string type, string location, DateTime time, string magnitude)
+        {
+            Disaster disaster = new Disaster(type, location, time, magnitude);
+            return disaster;
+        }
+
+    }
+}
---
+++ SafeDisaster/Class/Weather.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeDisaster.Class
+{
+    internal class Weather
+    {
+        private string _category;
+        private float _humidity;
+        private float _windVelocity;
+        private DateTime _time;
+
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+        public float Humidity
+        {
+            get { return _humidity; }
+            set { _humidity = value; }
+        }
+        public float WindVelocity
+        {
+            get { return _windVelocity; }
+            set { _windVelocity = value; }
+        }
+        public DateTime Time{
+            get { return _time; }
+            set { _time = value; }
+        }
+        public Weather(string category, float humidity, float windVelocity, DateTime time)
+        {
+            this.Category = category;
+            this.Humidity = humidity;
+            this.WindVelocity = windVelocity;
+            this.Time = time;
+        }
+        public Weather GetWeather(string category, float humidity, float windVelocity, DateTime time)
+        {
+            Weather weather = new Weather(category, humidity, windVelocity, time);
+            return weather;
+        }
+    }
+}
